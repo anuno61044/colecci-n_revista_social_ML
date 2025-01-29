23ef93753bdbc6ff2d74087dfcb99e9988f852a4
Marlon Díaz <m40331243@gmail.com>
2025-01-29 19:45:38 +1030

    fix: Repare merge damages

--- src/pipeline/image_extraction_flow.py
+++ src/pipeline/image_extraction_flow.py
@@ -1,7 +1,26 @@
+import os
 from prefect import flow
 
 
 @flow
 def extract_images_flow():
-    # Valida que existe la base de datos ya creada
-    pass
+    db = os.listdir("./external/dataset/data/output")
+    if len(db != 0):
+        return db[0]
+    
+    command = [
+        'python',
+        './image_text_sync/main.py',
+        './external/dataset/data/pdfs',
+        './external/dataset/data/images',
+        './external/dataset/data/captions',
+        './external/dataset/data/output/.db'
+    ]
+    try:
+        subprocess.run(command, check=True, text=True, capture_output=True)
+        db = os.listdir("./external/dataset/data/output")
+        return db[0]
+
+    except subprocess.CalledProcessError as e:
+        print("Error al ejecutar el comando")
+        print(e.stderr)
