6468303a27a84c5256244e9464bd98d1d0ded727
anuno61044 <anuno61044@gmail.com>
2025-01-30 14:54:55 -0500

    add cleaning scripts

--- src/cleaning_data/clean_metadata.py
+++ src/cleaning_data/clean_metadata.py
@@ -1,71 +1,66 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from transformers import CLIPModel, CLIPProcessor
 from PIL import Image
 import torch
 import lorem
+import os
+import sqlite3
 
 def clone_database():
     # Conectar a la base de datos original
     engine_original = create_engine('sqlite:///./external/dataset/metadata.db')
 
+    # Obtener las tablas de la base de datos original, excluyendo la tabla sqlite_sequence
+    tables = pd.read_sql("SELECT name FROM sqlite_master WHERE type='table' AND name != 'sqlite_sequence';", engine_original)
+
+    # Leer los datos de cada tabla en un DataFrame
+    dataframes = {}
+    for table in tables['name']:
+        dataframes[table] = pd.read_sql(f"SELECT * FROM {table}", engine_original)
+
+    # Ver los primeros datos de una tabla (por ejemplo, la primera tabla)
+    print(dataframes[tables['name'][0]].head())
+
     # Conectar a la base de datos clonada
     engine_copia = create_engine('sqlite:///./external/dataset/clean_metadata.db')
 
-    # Obtener los nombres de las tablas
-    tables = pd.read_sql("SELECT name FROM sqlite_master WHERE type='table';", engine_original)
-
-    # Recorrer cada tabla y copiarla a la nueva base de datos
-    for table in tables['name']:
-        df = pd.read_sql(f"SELECT * FROM {table};", engine_original)  # Leer la tabla original
-        df.to_sql(table, engine_copia, if_exists='replace', index=False)  # Guardar en la nueva BD
-
+    # Escribir los DataFrames en la nueva base de datos
+    for table, df in dataframes.items():
+        df.to_sql(table, engine_copia, index=False, if_exists='replace')
+    
+    # Cerrar el engine y liberar los recursos
+    engine_copia.dispose()
+    
     print("Base de datos clonada exitosamente.")
 
-def clean_database():
 
-    clone_database()
+# Cargar el modelo y el procesador de CLIP
+model_name = "openai/clip-vit-base-patch32"
+model = CLIPModel.from_pretrained(model_name)
+processor = CLIPProcessor.from_pretrained(model_name)
 
-    # Conectar a la base de datos
-    engine = create_engine('sqlite:///./external/dataset/clean_metadata.db')
-
-    # Leer los datos
-    metadata_df = pd.read_sql('ImageData', engine)
-
-    # Recorrer las filas verificando si caption es NULL o vacío
-    for index, row in metadata_df.iterrows():
-        image_path = row['image_path']
-        caption = row['caption']
-        
-        if not pd.isna(caption):
-        
-            # Definir el image_path que queremos modificar y el nuevo caption
-            image_path_to_modify, new_caption = evaluate_caption(image_path, caption)
-
-            # Modificar el caption en el DataFrame
-            metadata_df.loc[metadata_df['image_path'] == image_path_to_modify, 'caption'] = new_caption
-            
-            # Guardar los cambios en la base de datos
-            metadata_df.to_sql('ImageData', engine, if_exists='replace', index=False)
-            
 def evaluate_caption(image_path, caption):
 
-    if caption is '':
+    if caption == '':
         return caption
 
-    # Cargar el modelo y el procesador de CLIP
-    model_name = "openai/clip-vit-base-patch32"
-    model = CLIPModel.from_pretrained(model_name)
-    processor = CLIPProcessor.from_pretrained(model_name)
-
     # Lista de captions
-    captions = [caption]
+    captions = [caption[:70]]
 
     for i in range(5):
         captions.append(lorem.sentence())
 
+    if image_path.startswith('.\data\detected_images_1\\'):
+      new_image_path = image_path.replace('.\data\detected_images_1\\', '/external/dataset/data/detected_images_1/')
+    else:
+      new_image_path = image_path
+
+    if not os.path.exists(new_image_path):
+        return caption
+
     # Cargar y procesar la imagen
-    imagen = Image.open(image_path)
+    imagen = Image.open(new_image_path)
     inputs = processor(text= captions, images= [imagen]*len(captions), return_tensors="pt", padding=True)
 
     # Generar embeddings
@@ -77,12 +72,45 @@ def evaluate_caption(image_path, caption):
     captions_prob = list(zip(captions, probs[0]))
     captions_prob.sort(key=lambda x: x[1], reverse=True)
 
-    # Mostrar los captions y probabilidades ordenadas
+    # # Mostrar los captions y probabilidades ordenadas
     # for caption, prob in captions_prob:
     #     print(f"Caption: {caption} - Probabilidad: {prob:.4f}")
 
     if caption != captions_prob[0][0]:
-        print("Cambio de - ", caption)
+        # print(caption)
+        # print("------------------")
         return ''
     else:
         return caption
+            
+def clean_database():
+
+    clone_database()
+
+    # Conectar a la base de datos SQLite
+    conn = sqlite3.connect('./external/dataset/clean_metadata.db')
+    cursor = conn.cursor()
+
+    # Consultar las filas de la tabla ImageData
+    cursor.execute('SELECT image_path, caption FROM ImageData')
+
+    print('... Detectar malos captions ...')
+    # Recorrer las filas una por una
+    for row in cursor.fetchall():
+        image_path = row[0]  # 'image_path' está en la primera columna
+        caption = row[1]     # 'caption' está en la segunda columna
+        if caption == '':
+            continue
+        new_caption = evaluate_caption(image_path, caption)
+        if new_caption == caption:
+            continue
+        # Actualizar el caption en la base de datos para esta fila específica
+        cursor.execute('''
+            UPDATE ImageData
+            SET caption = ?
+            WHERE image_path = ?
+        ''', (new_caption, image_path))
+
+    # Guardar los cambios (commit) y cerrar la conexión
+    conn.commit()
+    conn.close()
